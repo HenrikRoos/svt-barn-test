efeabad599bc08e207ae23771dee5725baac00ff
Henrik Roos <henrik@afternoon.se>
2016-06-25 21:32:04 +0200

    Add svtibarn test

---
+++ test/svtibarn_test.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var By = require('selenium-webdriver').By;
+var test = require('selenium-webdriver/testing');
+var phantomjs = require('selenium-webdriver/phantomjs');
+// var chrome = require('selenium-webdriver/chrome');
+
+test.describe('Verifiera att det går att komma till barnplay-sidan från startsidan', function () {
+  this.timeout(20000);
+  var driver;
+  test.before(function () {
+    driver = new phantomjs.Driver();
+  });
+  test.after(function () {
+    driver.quit();
+  });
+  test.it('Kan komma till barnkanalen-sidan', function () {
+    driver.get('http://www.svt.se/barnkanalen');
+    assert(driver.isElementPresent(By.xpath('//div[@alt="Barnkanalen"]')), 'Kom inte fram till barnkanalen-sidan');
+  });
+  test.it('Kan trycka på barnplay knappen', function () {
+    driver.get('http://www.svt.se/barnkanalen');
+    var barnplayButtom = By.xpath('//a[@data-rt="link.mainNav.programao"]');
+    assert(driver.isElementPresent(barnplayButtom), 'Barnplay knappen finns inte');
+    driver.findElement(barnplayButtom).click();
+  });
+  test.it('Kom till barnplay sidan', function () {
+    var barnplay = driver.isElementPresent(By.id('__barnplay'));
+    assert(barnplay, 'Du kom inte fram till barnplay sidan');
+  });
+});
---
+++ typings/tsd.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="selenium-webdriver/selenium-webdriver.d.ts" />
